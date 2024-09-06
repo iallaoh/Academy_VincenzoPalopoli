0aa99880017a03e5cc3aacf1a74e87a9654d9dac
Vincenzo Palopoli <168841676+iallaoh@users.noreply.github.com>
2024-09-06 15:21:18 +0200

    Add files via upload

--- 6_09/area_trangolo.py
+++ 6_09/area_trangolo.py
@@ -1,5 +1,7 @@
 #area triangolo
 def area_triangolo(base, altezza):
+    base=int(input("qual è la base? "))
+    altezza=int(input("qual è l'altezza? "))
     return base*altezza/2
     
 #area quadrato
@@ -10,31 +12,52 @@ def area_quadrato(lato):
 def area_rettangolo(base, altezza):
     return base*altezza
 
-#lista
+#liste
 lista_risultati_triangolo=[]
 lista_risultati_quadrato=[]
 lista_risultati_rettangolo=[]
 
-
-while True:
-    scelta=int(input("qual è la figura di cui vuoi calcolare l'area? 1: triangolo, 2: quadrato, 3: rettangolo"))
+#
+def calcolo_area(scelta):
     if scelta==1:
         base=int(input("qual è la base? "))
         altezza=int(input("qual è l'altezza? "))
         risultato=area_triangolo(base, altezza)
+        print(f"il risultato è {risultato}")
         lista_risultati_triangolo.append(risultato)
     elif scelta==2:
         lato=int(input("qual è la base? "))
         risultato=area_quadrato(lato)
+        print(f"il risultato è {risultato}")
         lista_risultati_quadrato.append(risultato)
     elif scelta==3:
         base=int(input("qual è la base? "))
         altezza=int(input("qual è l'altezza? "))
         risultato=area_rettangolo(base, altezza)
+        print(f"il risultato è {risultato}")
         lista_risultati_rettangolo.append(risultato)
+    elif scelta==0:
+        print("Uscita dal programma.")
     else:
         print("errore")
-        continue
-    
-        
+
+
+#decoratore
+def decoratore_calcolo_area(iterazione):
+    def wrapper():
+        while True:
+            scelta=int(input("qual è la figura di cui vuoi calcolare l'area? 1: triangolo, 2: quadrato, 3: rettangolo, 0: esci"))
+            if scelta==0:
+                print("Grazie per aver usato il calcolatore di aree!")
+                break
+            calcolo_area(scelta)
+    return wrapper
+
+@decoratore_calcolo_area
+def calcolo_area_decorata(scelta):
+    calcolo_area(scelta)
+
+#eseguo la funzione        
+calcolo_area_decorata()
+
 
